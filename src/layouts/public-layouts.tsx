b0b675b267d9effb736b077eb149c8963ae02959
Aditya Mhaske <adityamhaske@visisense.in>
2025-03-08 23:43:48 +0530

    Firebase Conifgurations added

--- src/layouts/public-layouts.tsx
+++ src/layouts/public-layouts.tsx
@@ -2,9 +2,11 @@
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import { Outlet } from 'react-router-dom' 
+import AuthHanlder from "@/handlers/auth-handler";
 const PublicLayout = () => {
   return (
     <div className="w-full">
+      <AuthHanlder />
        <Header/>
          <Outlet/>
        <Footer/>
